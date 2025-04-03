97f46904ca089c4d1e31b300c9fda903cf9f3120
chococaker <152650318+chococaker@users.noreply.github.com>
2025-04-03 14:06:36 -0500

    Add Arduino implementation of AUS1

---
+++ src/arduino/aus1_controller.cpp
@@ -0,0 +1,170 @@
+/**
+ * Copyright 2025 John Jerney
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+#include "aus1_controller.h"
+
+#include "aus1.h"
+
+#include <cstdint>
+
+#define WIRE_TIMEOUT_ERR_CODE 5
+
+namespace superi2c {
+    aus1_controller::aus1_controller(TwoWire *wire)
+        : wire(wire),
+          state(aus1_controller_state::IDLE),
+          is_connected(false),
+          device_type(0),
+          device_version(0),
+          receiver(nullptr),
+          data(nullptr),
+          data_loc(0),
+          last_ping_ms(0),
+          last_bytes_received_ms(0) {}
+
+    bool aus1_controller::connected() const { return is_connected; }
+
+    void aus1_controller::set_timeout_period(unsigned long period) { this->timeout_period = period; }
+
+    void aus1_controller::request_data(receiver_function receiver) { this->receiver = receiver; }
+
+    aus1_controller_state aus1_controller::get_state() const { return state; }
+
+    void aus1_controller::update() {
+        unsigned long current_time = millis();
+
+        // Write wire data
+        if (wire->available()) {
+            last_bytes_received_ms = millis();
+        }
+        while (wire->available()) {
+            if (data_loc == data_buffer_size) break; // Discard overflowing buffer data
+            data[data_loc++] = wire->read(); 
+        }
+
+        // Failsafe: if state is IDLE and wire is receieving data, something has gone wrong.
+        // Await for the stream of data to end (hopefully it does) by waiting 10ms
+
+        if (state == aus1_controller_state::IDLE && current_time - last_bytes_received_ms <= 10) return;
+
+        // If the controller is not IDLE, then it must be receiving data.
+        // Assume the module was disconnected if the time the last bytes were receieved exceeds the timeout
+        if (state != aus1_controller_state::IDLE && current_time - last_bytes_received_ms > timeout_period) {
+            state = aus1_controller_state::IDLE;
+            is_connected = false;
+            reset(0);
+            return;
+        }
+        
+        switch (state) {
+            case aus1_controller_state::AWAITING_PING_RESPONSE:
+                if (data_loc == AUS1_PING_RESPONSE_PACKET_SIZE) {
+                    aus1_ping_response_packet packet = aus1_decode_ping_response(data);
+
+                    if (packet.peripheral_type == 0) { // invalid packet
+                        state = aus1_controller_state::IDLE;
+                        is_connected = false;
+                        receiver = nullptr;
+                        reset(0);
+                    } else {
+                        device_type = packet.peripheral_type;
+                        device_version = packet.peripheral_version;
+                        is_connected = true;
+                        last_ping_ms = current_time;
+                    }
+                }
+
+            break;
+
+            case aus1_controller_state::AWAITING_START_OF_STREAM:
+                if (data_loc == AUS1_START_OF_STREAM_PACKET_SIZE) {
+                    aus1_start_of_stream_packet packet = aus1_decode_start_of_stream(data);
+                    
+                    if (packet.data_size == 0) { // invalid packet
+                        state = aus1_controller_state::IDLE;
+                        is_connected = false;
+                        receiver = nullptr;
+                        reset(0);
+                        break;
+                    }
+
+                    received_data_size = packet.data_size;
+
+                    reset(packet.data_size + (packet.data_size % AUS1_DATA_PACKET_SIZE)); // set buffer to new size
+                    state = aus1_controller_state::RECEIVING_DATA;
+                }
+
+            break;
+            
+            case aus1_controller_state::RECEIVING_DATA:
+                if (data_buffer_size == data_loc) {
+                    receiver(data, received_data_size + (received_data_size % AUS1_DATA_PACKET_SIZE), data_buffer_size);
+
+                    // Remove data buffer without deleting it (which is what reset() does)
+                    data = nullptr;
+                    data_loc = 0;
+                    data_buffer_size = 0;
+
+                    receiver = nullptr;
+
+                    break;
+                }
+
+                if ((data_loc - 1) % AUS1_DATA_PACKET_SIZE == 0) {
+                    wire->requestFrom(AUS1_I2C_ADDRESS, AUS1_DATA_REQUEST_SIZE);
+                }
+                
+            break;
+
+            case aus1_controller_state::IDLE:
+                if (receiver != nullptr) { // a data retrieval is requested
+                    wire->requestFrom(AUS1_I2C_ADDRESS, AUS1_DATA_REQUEST_SIZE);
+                    reset(AUS1_START_OF_STREAM_PACKET_SIZE);
+                    state = aus1_controller_state::AWAITING_START_OF_STREAM;
+                } else if (current_time - last_ping_ms > 20) { // interval to ping is up
+                    uint8_t *packet = new uint8_t[AUS1_PING_PACKET_SIZE];
+                    aus1_encode_ping(packet);
+
+                    if (send_transmission(packet, AUS1_PING_PACKET_SIZE) == WIRE_TIMEOUT_ERR_CODE) {
+                        is_connected = false;
+                        state = aus1_controller_state::IDLE;
+                        break;
+                    }
+
+                    reset(AUS1_PING_RESPONSE_PACKET_SIZE);
+                    state = aus1_controller_state::AWAITING_PING_RESPONSE;
+                }
+
+            break;
+
+        }
+    }
+
+    void aus1_controller::reset(size_t new_buffer_size) {
+        data_loc = 0;
+        if (!data) delete[] data;
+        data = new uint8_t[new_buffer_size];
+        data_buffer_size = new_buffer_size;
+    }
+
+    int aus1_controller::send_transmission(uint8_t *buf, size_t len) {
+        wire->beginTransmission(AUS1_I2C_ADDRESS);
+        wire->write(buf, len);
+        return wire->endTransmission();
+    }
+}
---
+++ src/arduino/aus1_controller.h
@@ -0,0 +1,162 @@
+/**
+ * Copyright 2025 John Jerney
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+#pragma once
+
+#include "Wire.h"
+#include "aus1.h"
+
+namespace superi2c {
+    /**
+     * @brief Defines the state of an AUS1 controller
+     */
+    enum class aus1_controller_state {
+        AWAITING_PING_RESPONSE,
+        AWAITING_START_OF_STREAM,
+        RECEIVING_DATA,
+        IDLE
+    };
+
+    /**
+     * @brief A function that is called when data is received by a controller
+     */
+    typedef void (*receiver_function)(uint8_t *buf, size_t data_size, size_t buf_size);
+
+    class aus1_controller {
+        /**
+         * @brief Construct a new aus1 controller object
+         * 
+         * @param wire The I2C wire to take control of
+         */
+        explicit aus1_controller(TwoWire *wire);
+
+        /**
+         * @brief Gets the connection status of the controller wire
+         *
+         * 
+         * @return Whether the wire is connected to an AUS1 peripheral
+         */
+        bool connected() const;
+        
+        /**
+         * @brief Sets the period to wait before the peripheral is assumed to be disconnected
+         * 
+         * @param period The timeout period in milliseconds
+         */
+        void set_timeout_period(unsigned long period);
+
+        /**
+         * @brief Requests data from the peripheral
+         * 
+         * @param receiver The function to call when requested data is received.
+         */
+        void request_data(receiver_function receiver);
+
+        /**
+         * @brief Get the state object
+         * 
+         * @return aus1_controller_state The current state of this controller
+         */
+        aus1_controller_state get_state() const;
+
+        /**
+         * @brief Performs operations (reading, pinging, etc) that should be called every loop
+         */
+        void update();
+
+    private:
+        /**
+         * @brief The wire that is being commandeered by this controller
+         */
+        TwoWire *wire;
+        /**
+         * @brief The current state of the controller
+         */
+        aus1_controller_state state;
+        /**
+         * @brief Whether a peripheral is connected
+         */
+        bool is_connected;
+        
+        /**
+         * @brief The type of connected peripheral
+         */
+        uint32_t device_type;
+        /**
+         * @brief The version of the connected peripheral
+         */
+        uint16_t device_version;
+
+        /**
+         * @brief The function to be called when data is received after a request from a peripheral. `nullptr` when no data is being requested.
+         */
+        receiver_function receiver;
+        /**
+         * @brief 
+         */
+        uint32_t data_md5_hash;
+        /**
+         * @brief Size of the data being receieved
+         */
+        uint16_t received_data_size;
+
+        /**
+         * @brief The data buffer
+         */
+        uint8_t *data;
+        /**
+         * @brief The size of the data
+         */
+        size_t data_buffer_size;
+        /**
+         * @brief Current location of byte writer in the data buffer
+         */
+        size_t data_loc;
+
+        /**
+         * @brief The time it takes for the controller to time out and assume the peripheral to be disconnected
+         */
+        unsigned long timeout_period;
+
+        /**
+         * @brief The previous millisecond the peripheral was pinged
+         */
+        unsigned long last_ping_ms;
+        /**
+         * @brief The last millisecond data was receieved by the controller
+         */
+        unsigned long last_bytes_received_ms;
+        
+        /**
+         * @brief Deletes the data buffer, cleans up, and remakes it
+         * 
+         * @param new_buffer_size The size of the new data buffer
+         */
+        void reset(size_t new_buffer_size);
+        /**
+         * @brief Transmits some data to an AUS1 device across an I2C wire
+         * 
+         * @param wire The wire
+         * @param buf The data to write
+         * @param len The length of the data
+         * 
+         * @return The status of the transmission
+         */
+        int send_transmission(uint8_t *buf, size_t len);
+    };
+}
